396d1eb24273946dc4a4ca2851d7c716d9ed5f7f
planepaper <eddysonkr@gmail.com>
2023-03-18 00:47:57 +0900

    feat: Solve Greedy 1758

--- src/greedy/1758.cpp
+++ src/greedy/1758.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+vector<int> people;
+deque<int> tipQue;
+
 int main()
 {
     ios::sync_with_stdio(0);
@@ -15,9 +18,44 @@ int main()
 #endif
 
     int counts; cin >> counts;
+    people.reserve(counts);
 
+    for(int i = 0; i < counts; i++)
+    {
+        int num; cin >> num;
+        people.push_back(num);
+    }
 
+    sort(people.begin(), people.end());
 
+    for(int i = 0, order = 0; i < counts; i++)
+    {
+        if(people[i] >= order)
+        {
+            tipQue.push_back(people[i]);
+            order++;
+        }
+        else
+        {
+            tipQue.pop_front();
+            tipQue.push_back(people[i]);
+        }
+    }
+
+    while(tipQue.front() < (tipQue.size() - 1))
+    {
+        tipQue.pop_front();
+    }
+
+    long long result = 0;
+
+    for(auto tip : tipQue)
+    {
+        result += tip;
+    }
+    result -= (tipQue.size() - 1) * tipQue.size() / 2;
+
+    cout << result;
 
     return 0;
 }
